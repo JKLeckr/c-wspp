79382866caf41f704991cd4820d238232b8182ce
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2026-01-01 20:58:51 -0600

    Added test support

--- test/test.c
+++ test/test.c
@@ -4,10 +4,16 @@
 
 int main(int argc, char** argv)
 {
+    int res = 0;
     wspp_ws* ws = wspp_new("ws://localhost:38281");
     wspp_connect(ws);
-    for(;;) {
-        wspp_poll(ws);
+    int loop = 0;
+    while(!wspp_poll(ws)) {
+        if (loop > 5000) {
+            res = 1;
+            break;
+        }
     }
-    return 0;
+    wspp_close(ws, 0, "Done");
+    return res;
 }
